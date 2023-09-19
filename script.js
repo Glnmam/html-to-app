29dbd46181f0e9214fd7db6882be5aa1a4f1194e
Glenn <glenn.madzou.a.miere@etu.univ-poitiers.fr>
2023-09-19 18:02:54 +0200

    🚧 wewewewe🎉
    🚧 Sous titre

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.querySelector("h1").innerText += " from JS";
-// ou
-document.getElementById('title').style.color = 'red';   
+
+
+newFunction();   
+
+function newFunction() {
+    const title = document.getElementById('title');
+    document.querySelector("h1").innerText += " from JS";
+    // ou
+    document.getElementById('title').style.color = 'red';
+}
+const h2 = document.createElement("h2");
+h2.id = "sous-titre";
+h2.innerText = "Welcome to the javascript world";
+document.querySelector("h1").after(h2);
